60b4aca53639c489aa5782afda0bb28b96e188b1
GuoLin <mr.guo.lin@gmail.com>
2015-02-09 17:01:09 +0800

    change start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 
-#echo "install gulp && bower"
-#npm install gulp -g && npm install bower -g
+echo "install gulp"
+npm install gulp -g
 
-#echo "npm install && bower install"
-#npm install --production
-#bower install -f --allow-root
+echo "install bower"
+npm install bower -g
+
+echo "npm install && bower install"
+npm install --production
+bower install -f --allow-root
 
 export NODE_ENV=prodeuction
 node_modules/.bin/gulp dist
